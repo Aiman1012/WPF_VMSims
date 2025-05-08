f7b629531a1b398576aff73dd94948ef1f40acad
Aiman Aeffil <aimanaeffil1012@gmail.com>
2025-05-08 17:10:05 +0800

    Add project files.

---
+++ WPF_VMSims/MachinePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_VMSims
+{
+    /// <summary>
+    /// Interaction logic for MachinePage.xaml
+    /// </summary>
+    public partial class MachinePage : Window
+    {
+        public MachinePage()
+        {
+            InitializeComponent();
+        }
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
